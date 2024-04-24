ec5dc57bbd8e0093c5c388105acfacc73f393f2f
Mak Trnka <maktrnka@comcast.net>
2024-04-24 12:13:43 -0700

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,4 +1,12 @@
+import dynamoose from 'dynamoose';
+
+const Customers = dynamoose.model('customers', {
+  name: String,
+  age: Number
+});
+
 export const handler = async (event) => {
+  let
   console.log('THIS IS OUR REQUEST', event);
   // TODO implement
   const response = {
